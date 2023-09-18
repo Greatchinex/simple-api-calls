c4b53055a379f167b2112d54a8418a71b28df153
greatchinex <greatchinex@gmail.com>
2023-09-18 02:54:10 +0100

    non blocking gitthub star gazers

--- src/api_calls/non_blocking.rs
+++ src/api_calls/non_blocking.rs
@@ -1,8 +1,37 @@
-use reqwest::get;
+use reqwest::header::USER_AGENT;
+use reqwest::{get, Client};
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+pub struct User {
+    login: String,
+    id: u32,
+}
 
 #[tokio::main]
 pub async fn async_api_request() -> Result<String, anyhow::Error> {
     let res_body = get("http://httpbin.org/get").await?.text().await?;
 
     Ok(res_body)
+}
+
+#[tokio::main]
+pub async fn github_stargazers() -> Result<Vec<User>, anyhow::Error> {
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "rust-lang-nursery",
+        repo = "rust-cookbook"
+    );
+
+    println!("RequestUrl: {}", request_url);
+    let client = Client::new();
+    let res_data = client
+        .get(&request_url)
+        .header(USER_AGENT, "Rust web-api-client")
+        .send()
+        .await?;
+    let users: Vec<User> = res_data.json().await?;
+
+    Ok(users)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod api_calls;
 
 use api_calls::blocking::blocking_api_request;
-use api_calls::non_blocking::async_api_request;
+use api_calls::non_blocking::{async_api_request, github_stargazers};
 
 fn main() -> Result<(), anyhow::Error> {
     let res_body = blocking_api_request()?;
@@ -9,6 +9,9 @@ fn main() -> Result<(), anyhow::Error> {
 
     let res_body_async = async_api_request()?;
     println!("ASYNC_BODY: \n{}", res_body_async);
+
+    let star_gazers = github_stargazers()?;
+    println!("GITHUB REPO GAZERS: {:?}", star_gazers);
 
     Ok(())
 }
